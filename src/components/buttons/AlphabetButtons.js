07e5c1fcd850428a58e94a489eda96959c6555a7
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-02-01 18:21:18 +0000

    tidy

--- src/components/buttons/AlphabetButtons.js
+++ src/components/buttons/AlphabetButtons.js
@@ -1,19 +1,12 @@
 import React, { useState } from 'react';
-import { List, Button, Collapse, Icon } from 'antd';
+import { List, Button } from 'antd';
 
 import './AlphabetButtons.css';
-import CollapsePanel from 'antd/lib/collapse/CollapsePanel';
 
 const ALPHABET = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
-
-const collapsePanelStyle = {
-    background: 'white',
-    border: 0,
-    padding: '20px'
-}
 
 const AlphabetButtons = ({setFirstNameCharFilter}) => {
 
